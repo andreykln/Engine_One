8fdbe4f1149da0d0fdb0107beed30bc55b1122a4
Andrey Krivonogov <andreykrv88@gmail.ru>
2020-02-13 21:32:02 +0300

    in the process of creating swap chain and device

--- Graphics.cpp
+++ Graphics.cpp
@@ -1,1 +1,37 @@
 #include "Graphics.h"
+
+Graphics::Graphics(HWND wnd)
+{
+	DXGI_MODE_DESC displayModeDesc;
+	displayModeDesc.Height = 0u;
+	displayModeDesc.Width = 0u;
+	displayModeDesc.RefreshRate.Numerator = 60;
+	displayModeDesc.RefreshRate.Denominator = 1;
+	displayModeDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	displayModeDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	displayModeDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
+	swapChainDesc.BufferDesc = displayModeDesc;
+	swapChainDesc.SampleDesc.Count = 1u;
+	swapChainDesc.SampleDesc.Quality = 1u;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = DXGI_SWAP_EFFECT_SEQUENTIAL;
+	swapChainDesc.OutputWindow = wnd;
+	swapChainDesc.Windowed = FALSE;
+
+	IDXGIFactory::CreateSwapChain(gfx_pDevice, &swapChainDesc, &gfx_SwapChain);
+
+
+	D3D11CreateDeviceAndSwapChain(NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		D3D11_CREATE_DEVICE_DEBUG,
+		d3dFeatureLevels,
+		7u,
+		D3D11_SDK_VERSION,
+		&swapChainDesc,
+
+
+		)
+}
--- Graphics.h
+++ Graphics.h
@@ -10,5 +10,24 @@
 
 class Graphics
 {
+public:
+	Microsoft::WRL::ComPtr<ID3D11Device> gfx_pDevice;
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> gfx_pDeviceContext;
+	Microsoft::WRL::ComPtr<IDXGISwapChain> gfx_SwapChain;
+
+	Graphics(HWND wnd);
+
+
+private:
+	D3D_FEATURE_LEVEL d3dFeatureLevels [7]
+	{
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+		D3D_FEATURE_LEVEL_9_3,
+		D3D_FEATURE_LEVEL_9_2,
+		D3D_FEATURE_LEVEL_9_1,
+	};
 };
 
