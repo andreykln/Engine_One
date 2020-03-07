ce76409a540b4b445029625e8f0eb051313a140b
Andrey Krivonogov <andreykrv88@gmail.ru>
2020-03-07 10:16:54 +0300

    created indices buffer

--- Graphics.h
+++ Graphics.h
@@ -20,23 +20,35 @@ public:
 		struct Vertex
 		{
 			DirectX::XMFLOAT3 Pos;
-			DirectX::XMFLOAT3 Color;
+			DirectX::XMFLOAT4 Color;
 		};
 		const Vertex cubeCoord[]
 		{
 			DirectX::XMFLOAT3 {-1.0f, -1.0f, 0.0f},
-			DirectX::XMFLOAT3 {1.0f, 0.0f, 0.0f},
+			DirectX::XMFLOAT4 {1.0f, 0.0f, 0.0f,1.0f},
 			DirectX::XMFLOAT3 {1.0f, -1.0f, 0.0f},
+			DirectX::XMFLOAT4 {0.0f, 1.0f, 0.0f,1.0f},
 			DirectX::XMFLOAT3 {0.0f, 1.0f, 0.0f},
-			DirectX::XMFLOAT3 {0.0f, 1.0f, 0.0f},
-			DirectX::XMFLOAT3 {0.0f, 0.0f, 1.0f},
+			DirectX::XMFLOAT4 {0.0f, 0.0f, 1.0f,1.0f},
 		};
+
+		ID3D11InputLayout* pInputLayout;
+		const D3D11_INPUT_ELEMENT_DESC inputElemDesc[] =
+		{
+			{"Position", 0u, DXGI_FORMAT_R32G32_FLOAT, 0u, D3D11_APPEND_ALIGNED_ELEMENT,
+			D3D11_INPUT_PER_VERTEX_DATA, 0u},
+			{"Color", 0u, DXGI_FORMAT_R32G32B32A32_FLOAT, 0u, D3D11_APPEND_ALIGNED_ELEMENT,
+			D3D11_INPUT_PER_VERTEX_DATA, 0u}
+		};
+		pgfx_pDevice->CreateInputLayout(inputElemDesc, 2u, , , &pInputLayout);
+
 		D3D11_BUFFER_DESC bufferDesc;
 		bufferDesc.Usage = D3D11_USAGE_DEFAULT;
-		bufferDesc.ByteWidth = sizeof(Vertex) * 3;
+		bufferDesc.ByteWidth = sizeof(Vertex) * 6;
 		bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
 		bufferDesc.CPUAccessFlags = 0;
 		bufferDesc.MiscFlags = 0;
+		bufferDesc.StructureByteStride = 0u;
 		// Fill in the sub resource data.
 		D3D11_SUBRESOURCE_DATA initData;
 		initData.pSysMem = cubeCoord;
@@ -46,15 +58,40 @@ public:
 
 		DX::ThrowIfFailed(pgfx_pDevice->CreateBuffer(&bufferDesc, &initData, pVertexBuffer.GetAddressOf()));
 
-		D3D11_INPUT_ELEMENT_DESC inputElemDesc[] =
+		const UINT indices[3]
 		{
-			"Position", 0u, DXGI_FORMAT_R32G32_FLOAT, 0u, D3D11_APPEND_ALIGNED_ELEMENT,
-			D3D11_INPUT_PER_VERTEX_DATA, 0u
+			0,1,2
 		};
 
-		UINT stride = sizeof(Vertex);
-		UINT offset = 0;
+		D3D11_BUFFER_DESC indicesBuffDesc;
+		indicesBuffDesc.Usage = D3D11_USAGE_IMMUTABLE;
+		indicesBuffDesc.ByteWidth = sizeof(UINT) * 3;
+		indicesBuffDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+		indicesBuffDesc.CPUAccessFlags = 0u;
+		indicesBuffDesc.MiscFlags = 0u;
+		indicesBuffDesc.StructureByteStride = 0u;
+		D3D11_SUBRESOURCE_DATA indicesInitData;
+		indicesInitData.pSysMem = indices;
+		indicesInitData.SysMemPitch = 0u;
+		indicesInitData.SysMemSlicePitch = 0u;
+		ID3D11Buffer* pIndicesBuffer;
+		DX::ThrowIfFailed(pgfx_pDevice->CreateBuffer(&indicesBuffDesc, &indicesInitData, &pIndicesBuffer));
+		pgfx_pDeviceContext->IASetIndexBuffer(pIndicesBuffer, DXGI_FORMAT_R32_UINT, 0u);
+		
+
+		const UINT stride = sizeof(Vertex);
+		const UINT offset = 0;
 		pgfx_pDeviceContext->IASetVertexBuffers(0u, 1u, &pVertexBuffer, &stride, &offset);
+
+	
+		
+		
+	
+
+
+
+
+
 	}
 
 private:
@@ -66,7 +103,7 @@ private:
 
 	D3D_FEATURE_LEVEL featureLevelIsSupported;
 	UINT featureLevelNum = 7;
-	D3D_FEATURE_LEVEL d3dFeatureLevels [7]
+	const D3D_FEATURE_LEVEL d3dFeatureLevels [7]
 	{
 		D3D_FEATURE_LEVEL_11_1,
 		D3D_FEATURE_LEVEL_11_0,
